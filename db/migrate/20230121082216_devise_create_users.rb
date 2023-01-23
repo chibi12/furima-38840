2f410c706da56e468744de99df5c0b876fb3c54a
chibi12 <yukky.countrytraveler12@au.com>
2023-01-23 16:45:19 +0900

    サインアップ・新規登録

--- db/migrate/20230121082216_devise_create_users.rb
+++ db/migrate/20230121082216_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name,          null: false, defalt:  ""
       t.string :first_name_kana,    null: false, defalt:  ""
       t.string :last_name_kana,     null: false, defalt:  ""
-      t.date   :birthday,           null: false
+      t.date   :birthday,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
