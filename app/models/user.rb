b357dc456afe130395bea68f6dfccc5c3d314443
chibi12 <yukky.countrytraveler12@au.com>
2023-01-25 17:22:06 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
-         validates :birthday, presence: true
-         validates :email, presence: true
-         validates :encrypted_password, presence: true
 
+         validates :nickname, presence: true, length: { minimum: 6 }
+         with_options presence: true do
+            validates :nickname, :birthday, :email, :password_confirmation
+            validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+            validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
+         end
+        validates :password, :password_confirmation,
+        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数で入力して下さい" }
 end
