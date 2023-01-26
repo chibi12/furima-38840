ab9933364eaebdd0374bfb0a669ce8c8eb3a198a
chibi12 <yukky.countrytraveler12@au.com>
2023-01-26 17:05:25 +0900

    データ移動のため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,10 @@
 class ItemsController < ApplicationController
-    def index
-        
-    end
 
-    def new
-        #@item = Item. new
-    end
+  def index
+    #@items = Item.all
+  end
 
-
+  def new
+    @item = Item.new
+  end
 end
