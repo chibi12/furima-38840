4080d6e36858e8ecb4f7eb27abdf027c3ef14515
chibi12 <yukky.countrytraveler12@au.com>
2023-01-27 11:36:16 +0900

    事前準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
     belongs_to :user
     has_many :purchases
+    has_one_attached :image
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :genre
+
+    validates :name, :price, :comment, presence: true
+    validates :genre_id, numericality: { other_than: 1, message: "can't be blank"} 
 end
