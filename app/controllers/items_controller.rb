6dd0c2038f60c90698ac6212fd4c97aa5ef0bdf7
chibi12 <yukky.countrytraveler12@au.com>
2023-02-02 12:22:59 +0900

    routing,controllerの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,6 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.order(id: :DESC)
-
   end
 
   def new
@@ -23,6 +22,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
   private
 
   def item_params
