783786c73ffc387a90f4de98d6a545bf40494e1e
chibi12 <yukky.countrytraveler12@au.com>
2023-02-07 19:53:56 +0900

    routingとcontrollerの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,32 +2,32 @@ class OrdersController < ApplicationController
      before_action :authenticate_user!, except: :index
 
     def index
-        @item = Item.find(params[:item_id])
-        @order = Order.new
+      @item = Item.find(params[:item_id])
+      @order_address = OrderAddress.new
     end
         
-    def new
-        @order_address = OrderAddress.new
-    end
+    # def new
+    #     @order_address = OrderAddress.new
+    # end
 
     def create
-      @order_address = OrderAddress.new(order_params)
+      @item = Item.find(params[:item_id])
+      @order_address = OrderAddress.new(order_address_params)
       if @order_address.valid?
          @order_address.save
          redirect_to root_path
       else
-         render :new
+         render :index
       end
     end
 
     private
 
-  def order_params
-    params.permit(user_id: current_user.id, item_id:@item.id)
+  def order_address_params
+    params.require(:order_address).permit(:post_number, :sender_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 
-  def address_params
-    params.permit(:post_number, :sender_id, :city, :addresses, :building, :phone_number).merge(order_id: @order.id)
-  end
-
-end
+  # def address_params
+  #   params.permit(:post_number, :sender_id, :city, :addresses, :building, :phone_number).merge(order_id: @order.id)
+  # end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :items do
     root "orders#index"
-    resources :orders, only: [:index, :new, :create]
+    resources :orders, only: [:index, :create]
     end
     
 end
