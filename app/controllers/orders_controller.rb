6be8cc339600be969d6e55923d0f328c208d18f8
chibi12 <yukky.countrytraveler12@au.com>
2023-02-09 22:04:57 +0900

    追加修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
-     before_action :authenticate_user!, except: :index
-     before_action :contributor_confirmation
+     before_action :authenticate_user!, only: [:index, :create]
+     before_action :non_order_item,only: [:index, :create]
 
     def index
       @item = Item.find(params[:item_id])
@@ -26,7 +26,7 @@ class OrdersController < ApplicationController
     private
 
   def order_address_params
-    params.require(:order_address).permit(:post_number, :sender_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_address).permit(:post_number, :sender_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   # def address_params
@@ -36,11 +36,13 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
     Payjp::Charge.create(  
-      card: order_params[:token],    
+      amount: @item.price,
+      card: order_address_params[:token],    
       currency: 'jpy'                 
     )
   end
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @item.user 
+  def non_order_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user == @item.user || @item.order.present?
   end
  end
